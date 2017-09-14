a30b9386cba6195d28cacff1de4ebdea738c833c
Tomlezen <t.nianshang@foxmail.com>
2017-09-14 18:35:07 +0800

    整理代码

--- lib/src/main/java/com/tlz/extensions/Toasts.kt
+++ lib/src/main/java/com/tlz/extensions/Toasts.kt
@@ -5,18 +5,18 @@ import android.content.Context
 import android.widget.Toast
 
 
-fun Fragment.toast(message: Int): Unit = activity.toast(message)
+fun Fragment.toast(message: Int) = activity.toast(message)
 
 fun Context.toast(message: Int) = Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
 
-fun Fragment.toast(message: CharSequence): Unit = activity.toast(message)
+fun Fragment.toast(message: CharSequence) = activity.toast(message)
 
 fun Context.toast(message: CharSequence) = Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
 
-fun Fragment.longToast(message: Int): Unit = activity.longToast(message)
+fun Fragment.longToast(message: Int) = activity.longToast(message)
 
 fun Context.longToast(message: Int) = Toast.makeText(this, message, Toast.LENGTH_LONG).show()
 
-fun Fragment.longToast(message: CharSequence): Unit = activity.longToast(message)
+fun Fragment.longToast(message: CharSequence) = activity.longToast(message)
 
 fun Context.longToast(message: CharSequence) = Toast.makeText(this, message, Toast.LENGTH_LONG).show()
